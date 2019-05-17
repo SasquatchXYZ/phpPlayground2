559bbfaed724bef5beb13cd32f23091baa1c95da
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-05-16 23:45:47 -0400

    WIP Chapter 2 Exercises

--- future_value_calculator/display_results.php
+++ future_value_calculator/display_results.php
@@ -1,20 +1,48 @@
 <?php
   // Get the Data from the Form
+  $investment = filter_input(INPUT_POST, 'investment', FILTER_VALIDATE_FLOAT);
+  $interest_rate = filter_input(INPUT_POST, 'interest_rate', FILTER_VALIDATE_FLOAT);
+  $years = filter_input(INPUT_POST, 'years', FILTER_VALIDATE_INT);
 
   // Validate Investment
+  if ($investment === FALSE) {
+    $error_message = 'Investment must be a valid number.';
+  } else if ($investment <= 0) {
+    $error_message = 'Investment must be greater than 0.';
 
-  // Validate Interest Rate
-
-  // Validate Years
-
-  // Set Error Message to empty string if no invalid entries
+    // Validate Interest Rate
+  } else if ($interest_rate === FALSE) {
+    $error_message = 'Interest Rate must be a valid number.';
+  } else if ($interest_rate <= 0) {
+    $error_message = 'Interest Rate must be greater than zero';
+    // Validate Years
+  } else if ($years === FALSE) {
+    $error_message = 'Years must be a valid whole number.';
+  } else if ($years <= 0) {
+    $error_message = 'Years must be greater than zero.';
+  } else if ($years > 30) {
+    $error_message = 'Years must be less than 31.';
+    // Set Error Message to empty string if no invalid entries
+  } else {
+    $error_message = '';
+  }
 
   // If an error message exists, go to the index page
+  if ($error_message != '') {
+    include('index.php');
+    exit();
+  }
 
   // Calculate the future value
+  $future_value = $investment;
+  for ($i = 1; $i <= $years; $i++) {
+    $future_value += $future_value * $interest_rate * 0.01;
+  }
 
   // Apply currency and percent formatting
-
+  $investment_f = '$' . number_format($investment, 2);
+  $yearly_rate_f = $interest_rate . '%';
+  $future_value_f = '$' . number_format($future_value, 2);
 ?>
 
 <!DOCTYPE html>
