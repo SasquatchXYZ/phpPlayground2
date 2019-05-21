ff63e8f358ada4bc8974e1c874d2350a45672371
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-05-20 23:10:27 -0400

    WIP completed product_manager application

---
+++ product_manager/error.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<!-- The Head Section -->
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale = 1.0">
+
+  <title>My Guitar Shop</title>
+  <link href="main.css" rel="stylesheet" type="text/css">
+
+</head>
+<!-- The Body Section -->
+<body>
+<header><h1>My Guitar Shop</h1></header>
+
+<main>
+  <h2 class="top">Error</h2>
+  <p><?php echo $error; ?></p>
+</main>
+
+<footer>
+  <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+</footer>
+</body>
+</html>
